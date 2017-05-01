0a5c1c19a425a371cb8b6a8e18781769b9487bcd
Angel Lopez <mrlopez@stanford.edu>
2017-05-01 12:01:01 -0700

    adding d3 and a main html file to start off, considering adding some angular after this commit

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+window.onload = function(){
+   var addNodeButton = document.getElementById('addNodeButton');
+   addNodeButton.addEventListener("click", function(){
+      alert('This is an example of how to add events');
+   });
+}
