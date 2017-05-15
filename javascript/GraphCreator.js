29d0f492d493b6a39050bf35948d3b5f3e323297
devela <devela94@gmail.com>
2017-05-15 09:49:13 -0700

    Update GraphCreator.js
    removed (redundant) debugging code

--- javascript/GraphCreator.js
+++ javascript/GraphCreator.js
@@ -157,7 +157,7 @@ GraphCreator.prototype.bfs = function(start, goal){
 
 // Possible TODO: use IDs as parameters (instead of nodes)
 GraphCreator.prototype.dfs = function(start, goal){
-	var set = Set([]);
+	var set = new Set([]);
 	set.push(start);
 	var change = new StateChange();
 	change.addChangedNode(start, "green");
@@ -184,43 +184,6 @@ GraphCreator.prototype.dfs = function(start, goal){
 	}
 };
 
-function Edge(from, to){
-   this.from = from;
-   this.to = to;
-}
-
-Edge.prototype.switchDirection = function(){
-   var temp = this.from;
-   this.from = this.to;
-   this.to = temp;
-};
-
-function Node(value, weight, color, id){
-    // this.x = x;
-    // this.y = y;
-    this.value = value;
-    this.id = id;
-    this.weight = weight;
-    this.color = color;
-    this.intermediateValue = null;
-    this.visited = false;
-    // We require two neighbor sets for directed graphs.
-    // For undirected graphs, we will only use the "out" fields
-    this.in_neighbors = new Set([]);	// nodes pointing to current
-    this.out_neighbors = new Set([]);	// current points to these
-    // the corresponding edges for the above node lists
-    this.in_edges = new Map([]);	// map: node ID -> edge
-    this.out_edges = new Map([]);
-    this.parent = null;
-    // this.ends = new Set([]);
-}
-
-// clears all values relevant to algorithmic side of things
-Node.prototype.node_reset = function(){
-   this.visited = false;
-   this.intermediateValue = null;
-   this.weight = 0;
-};
 // given a Node, highlights the node/updates graph
 // selectNode (given x,y)
 
