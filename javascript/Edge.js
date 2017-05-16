2b831a4e9f23f84a9875c5e7b30adbdae793b6a8
devela <devela94@gmail.com>
2017-05-16 14:17:04 -0700

    Update Edge.js
    Renamed to/from to end/start for consistency

--- javascript/Edge.js
+++ javascript/Edge.js
@@ -1,18 +1,18 @@
 'use strict';
 
-function Edge(from, to){
+function Edge(start, end){
    // if(a === b){
    //    ;//TODO: assert error
    // }
-   this.from = from;
-   this.to = to;
+   this.start = start;
+   this.end = end;
 }
 
 // NOTE: the switchDirection operation is local to the edge structure.
 // In particular, this does not change the lists in which this edge apears,
 // so the edge must be swapped in any in/out lists it appears in
 Edge.prototype.switchDirection = function(){
-   var temp = this.from;
-   this.from = this.to;
-   this.to = temp;
+   var temp = this.start;
+   this.start = this.end;
+   this.end = temp;
 };
