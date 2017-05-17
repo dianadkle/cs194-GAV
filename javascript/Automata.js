4b6eb84069c09d051049dd15182892e3f072e5c1
Diana Le <dianale@DN0a24633a.SUNet>
2017-05-16 17:54:12 -0700

    added automata.js class

---
+++ javascript/Automata.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// always create an empty graph
+function Automata(regex){
+	console.log("Created Automata object");
+	// a removed node will be replaced by the value -1
+	this.regex = regex;
+	this.nfa = null;
+	this.dfa = null;
+
+}
+
+Automata.prototype.convertToNFA = function() {
+	var regParser = require('automata.js');
+	var parser = new regParser.RegParser(this.regex);
+
+	var nfa = parser.parseToNFA();
+
+	this.nfa = nfa;
+
+	console.log(nfa);
+
+	return nfa;
+};
+
+
+Automata.prototype.convertToDFA = function() {
+	var regParser = require('automata.js');
+	var parser = new regParser.RegParser(this.regex);
+
+	var dfa = parser.parseTODFA();
+
+	this.dfa = dfa;
+
+	console.log(dfa);
+
+	return dfa;
+	
+};
+
+// IN ORDER TO USE THIS FUNCTION, YOU MUST FIRST CONVERT THE REGEX TO A DFA
+Automata.prototype.stringMatches = function(text) {
+	return this.dfa.match(text);
+};
+
+
