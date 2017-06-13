d88e50892c9d696909b7a577fa4d4c32579a3fdc
Angel Lopez <mrlopez@stanford.edu>
2017-06-13 14:54:58 -0700

    started saved graphs stuff

--- javascript/controllers/SavedGraphController.js
+++ javascript/controllers/SavedGraphController.js
@@ -8,22 +8,29 @@ function SavedGraphController(userInfo){
    this.userInfo = userInfo;
 };
 
-SavedGraphController.prototype.control = function(){
+SavedGraphController.prototype.control = function(index){
 	var user;
 	var u_name = this.userInfo.username;
 	var graphs;
     $.get("http://127.0.0.1:3000", function (data) {
 	    for (var i = 0; i < data.length; i++){
-	        console.log("data:"+data[i].username);
-	        console.log("profile:"+u_name);
 	        if (data[i].username === u_name){
 	            user = data[i];
-	        } 
+	        }
 	    }
 	    graphs = user.graphs;
+       var i = 1;
 	    for (var property in graphs) {
     		if (graphs.hasOwnProperty(property) && property !== "key") {
-        		//display graph names
+            var id= "graph" + i;
+            var imgID_hash = '#graphImage' + i;
+            var imgID = 'graphImage' + i;
+            $(imgID_hash).css("display", "inline");
+            document.getElementById(id).innerHTML = property;
+        		i++;
+            document.getElementById(imgID).onclick = function(){
+               index.renderGraph(graphs[property]);
+            };
     		}
 		}
     });
